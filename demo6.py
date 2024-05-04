99a5c687a7ab4ebad14a599dc70a4c36c50ac337
长久者 <109289015+cjz-wr@users.noreply.github.com>
2024-05-04 23:19:35 +0800

    Update demo6.py

--- demo6.py
+++ demo6.py
@@ -1,6 +1,6 @@
 import input_chinese
 oled_controller = input_chinese.OLEDController()#创建实例
-oled_controller.display_chinese_on_oled("你好世界！")
+oled_controller.display_chinese_on_oled("你好word",0,20) #后面的为显示位置
 # # 创建OLED控制器实例，并指定SCL和SDA引脚
 # oled_controller = OLEDController(scl_pin=22, sda_pin=21)
 
